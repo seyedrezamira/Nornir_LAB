b5a54874aa766c56230e02c954543ec0096027cd
Seyedreza <reza.mira@live.com>
2022-07-12 16:24:00 +0200

    Add file 28 and modified some files

--- 001-showver_test-genie.py
+++ 001-showver_test-genie.py
@@ -10,15 +10,17 @@ nr = InitNornir(config_file="config.yaml")
 hosts_dict = {}
 
 def show_command(task):
-    showver = task.run(task=netmiko_send_command, command_string="sho license usage", use_genie=True)
+    showver = task.run(task=netmiko_send_command, command_string="show int status", use_genie=True)
     task.host["items"] = showver.result
+    list1 = list(task.host["items"]["interfaces"])
+    print (list1)
     # version = task.host["items"]["platform"]["software"]["system_version"]
     # hosts_dict[task.host] = version
 
 
 result = nr.run(task=show_command)
-print_result(result)
 #pprint(hosts_dict)
+print_result(result)
 
-# import ipdb
-# ipdb.set_trace()
+import ipdb
+ipdb.set_trace()
---
+++ 027-show_vlan.py
@@ -0,0 +1,24 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+from pprint import pprint
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+
+def show_vlan(task):
+    show_vlan = task.run(task=send_command, command="show interface status")
+    task.host["items"] = show_vlan.scrapli_response.genie_parse_output()
+    
+    int_list = list(task.host["items"]["interfaces"])
+    rprint (f"{task.host} interface list:")
+    for interface in int_list:
+        vlan_id = task.host["items"]["interfaces"][interface]["vlan"]
+        if vlan_id == "trunk":
+            rprint (f"{interface}: is trunk")
+
+   
+result = nr.run(task=show_vlan)
+#print_result(result)
+# import ipdb
+# ipdb.set_trace()
