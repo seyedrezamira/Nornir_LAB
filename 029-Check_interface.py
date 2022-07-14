65f71e935bc4bbf5e436b46263fa2839d397bafd
Seyedreza <reza.mira@live.com>
2022-07-14 10:29:26 +0200

    add 29,30 files

---
+++ 029-Check_interface.py
@@ -0,0 +1,23 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command
+from nornir_utils.plugins.functions import print_result
+from pprint import pprint
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+dev_name_dic = {}
+dev_vlan_dic = {}
+
+def show_interface_status(task):
+    show_interface_status = task.run(task=send_command, command="show interface status")
+    task.host["items"] = show_interface_status.scrapli_response.genie_parse_output()
+    int_list = list(task.host["items"]["interfaces"])
+    for interface in int_list:
+        vlan_id = task.host["items"]["interfaces"][interface]["vlan"]
+        dev_vlan_dic[interface]= vlan_id
+    dev_name_dic[task.host] = dev_vlan_dic
+
+result = nr.run(task=show_interface_status)
+rprint(dev_name_dic)
+import ipdb
+ipdb.set_trace()
---
+++ 030-Set-CDP_neighbor.py
@@ -0,0 +1,23 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_command,send_configs
+from nornir_utils.plugins.functions import print_result
+from pprint import pprint
+from rich import print as rprint
+
+nr = InitNornir(config_file="config.yaml")
+
+def show_interface_status(task):
+    show_interface_status = task.run(task=send_command, command="show cdp neighbor")
+    task.host["items"] = show_interface_status.scrapli_response.genie_parse_output()
+    # cdp = task.host["items"]["cdp"]["index"]
+    # for neighbor in cdp:
+    #     local_int = cdp[neighbor]["local_interface"]
+    #     remote_port = cdp[neighbor]["port_id"]
+    #     remote_dev = cdp[neighbor]["device_id"]
+    #     config_commands = [f"interface {local_int}",f"desc connected to {remote_dev} via {remote_port}"]
+    #     task.run(send_configs, configs=config_commands)
+
+
+result = nr.run(task=show_interface_status)
+import ipdb
+ipdb.set_trace()
