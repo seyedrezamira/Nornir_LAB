85427e6bc277828dd3fbedc38ab944725dcc44bb
Seyedreza Mira <seyedreza.mira@ionos.com>
2022-07-22 16:51:22 +0200

    37 complete before mac learn

--- 37-Genie_U230-DCS.py
+++ 37-Genie_U230-DCS.py
@@ -8,31 +8,35 @@ nr = InitNornir(config_file="config.yaml")
 def U230_DCS(task):
     vlan_status = task.run(task=send_command, command="show vlan")
     task.host["items"] = vlan_status.scrapli_response.genie_parse_output()
-    
     interface_status = task.run(task=send_command, command="show interface")
     task.host["attr"] = interface_status.scrapli_response.genie_parse_output()
-    
     vlan_list = list(task.host["items"]["vlans"])
-    dic = {task.host:vlan_list}
     
-    # rprint(dic)
+    
+    vlan_num_dic = {}
+    int_status_dic = {}
+    output_dic = {}
+    vlan_list_dic = {}
+    
+    vlan_list_dic[task.host]=vlan_list
+    rprint(vlan_list_dic)
 
-    # rprint(f"\nThe list of Vlan in {task.host} is --> {vlan_list}")
-        
+    output_dic[task.host]=vlan_num_dic
     for vlan_num in vlan_list:
-        if vlan_num  != "1":
+        if (vlan_num != "1"):
             interface_per_vlan = task.host["items"]["vlans"][vlan_num]["interfaces"]
-            dic[vlan_num] = interface_per_vlan
-            # rprint (f"\nVLAN {vlan_num} is set on: {interface_per_vlan}")
-    rprint(dic)
-            # for int in interface_per_vlan:
-            #     if not str(int).startswith("Port"):
-            #         admin_state= task.host["attr"][int]["admin_state"]
-            #         oper_state= task.host["attr"][int]["oper_status"]
-            #         rprint (f"\n{int}:\nadmin state is {admin_state}\noperation status is {oper_state}\n")
+            
+            int_status_dic = {}
+            for int in interface_per_vlan:
+                if not str(int).startswith("Port") and task.host["attr"][int]["port_mode"]=="access":
+                    admin_state= task.host["attr"][int]["admin_state"]
+                    oper_state= task.host["attr"][int]["oper_status"]
+                    int_status_dic[int]=[admin_state,oper_state]
+            vlan_num_dic[vlan_num]=int_status_dic
+    rprint(output_dic)
 
 
 result = nr.run(task=U230_DCS)
 
-# import ipdb
-# ipdb.set_trace()
+import ipdb
+ipdb.set_trace()
