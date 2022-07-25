c5ca0285bc806e5f2916d2e4cf59a96605bdc002
Seyedreza Mira <seyedreza.mira@ionos.com>
2022-07-25 09:51:15 +0200

    modified hosts for automation

---
+++ 37-JinjaTemp_loadvar.py
@@ -0,0 +1,25 @@
+from nornir import InitNornir
+from nornir_scrapli.tasks import send_configs
+from nornir_utils.plugins.functions import print_result
+from nornir_jinja2.plugins.tasks import template_file
+from nornir_utils.plugins.tasks.data import load_yaml
+
+nr = InitNornir(config_file="config.yaml")
+
+def load_var(task):
+    data = task.run(task=load_yaml, file=f"./host_vars/{task.host}.yaml")
+    task.host["facts"] = data.result
+    test_template(task)
+
+def test_template(task):
+    template = task.run(task=template_file, template="snmp.j2", path=f"{task.host.platform}-templates")
+    task.host["snmp_config"] = template.result
+    rendered = task.host["snmp_config"]
+    configuration = rendered.splitlines()
+    task.run(task=send_configs, configs=configuration)
+
+
+
+results = nr.run(task=load_var)
+print_result(results)
+
